8ab7ff1d00025ebaf6defd73354a9c1aff2dbf17
smmasudrana <smmasudrana62@gmail.com>
2025-09-14 12:41:26 +0600

    add search functionality

--- script.js
+++ script.js
@@ -126,3 +126,21 @@ const displayLesson =(lessons) =>{
 }
 
 loadLesson(); 
+
+
+
+
+//------------------
+document.getElementById('btn-search').addEventListener('click', () =>{
+    removeActive();
+    const input=document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase();
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then(res => res.json())
+    .then((data) =>{
+        const allWords =data.data;
+        const filterWords = allWords.filter((word) => word.word.toLowerCase().includes(searchValue));
+        displayLevelWord(filterWords);
+    })
+})
